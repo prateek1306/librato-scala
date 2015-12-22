7df8723f028fc03f0ca42fb09423c3e707b35461
Prateek Shrivastava <13.prateek@gmail.com>
2015-12-22 15:24:12 +0530

    Adds setter for time in Gauge class

--- src/main/scala/com/urlkin/librato/core/Gauge.scala
+++ src/main/scala/com/urlkin/librato/core/Gauge.scala
@@ -6,8 +6,14 @@ import org.codehaus.jettison.json.JSONObject
   * Created by prateek on 15/12/15.
   */
 class Gauge(name: String, value: Number) extends Measurement(name = name, value = value) {
+
   override def toJSON: JSONObject = {
     val json = super.toJSON
     json
+  }
+
+  def setTime(time: Long): Gauge = {
+    measureTime = time
+    this
   }
 }
