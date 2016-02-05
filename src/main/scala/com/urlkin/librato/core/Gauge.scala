5683fa0e31a7448209435cf4aad8a44bb4d89af3
Prateek Shrivastava <13.prateek@gmail.com>
2016-02-05 21:36:36 +0530

    Adds gauge specific properties

--- src/main/scala/com/urlkin/librato/core/Gauge.scala
+++ src/main/scala/com/urlkin/librato/core/Gauge.scala
@@ -7,8 +7,14 @@ import org.codehaus.jettison.json.JSONObject
   */
 class Gauge(name: String, value: Number) extends Measurement(name = name, value = value) {
 
+  var summarization: String = null
+  var shouldAggregate: Boolean = false
+
   override def toJSON: JSONObject = {
     val json = super.toJSON
+    if (summarization != null)
+      json.put("summarize_function", summarization)
+    json.put("aggregate", shouldAggregate)
     json
   }
 
@@ -16,4 +22,27 @@ class Gauge(name: String, value: Number) extends Measurement(name = name, value 
     measureTime = time
     this
   }
+
+  def setSummarisation(value: String): Gauge = {
+    this.summarization = value
+    this
+  }
+
+  def aggregate: Gauge = {
+    this.shouldAggregate = true
+    this
+  }
+
+  def setPeriod(value: Int) = {
+    this.period = value
+    this
+  }
+}
+
+object GaugeSummarization {
+  val AVERAGE = "average"
+  val SUM = "sum"
+  val COUNT = "count"
+  val MIN = "min"
+  val MAX = "max"
 }
